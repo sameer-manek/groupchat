f3dbb76c2f673a52d7abb63dcb269c7f8af814ac
_sameermanek <sameer@moodcafe.in>
2018-08-13 10:34:09 +0530

    update > people can now join the chat

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const server = require('http').Server(app)
 
 var io = require('socket.io')(server)
 
+var users = ['System', ];
+
 server.listen(3000, () => console.log("server started on port 3000"))
 
 app.get('/', (req, res) => res.sendFile(path.resolve('./views/index.html')))
 
 io.on('connection', function(socket){
-	socket.emit("init", {msg: "io is working!"});
+	socket.emit("init", { user: users[0], msg: "io is working!" });
 	socket.on("msg", (data) => socket.broadcast.emit("new message", data))
+	socket.on("new user", (data) => {
+		// add the user to userlist
+		users.push(data.username)
+		console.log(users)
+	})
 });
